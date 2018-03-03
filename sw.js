bb17cfd7f92293dc44e89b55ffe1681b882457a5
Jason <Jpihlk@yahoo.com>
2018-03-03 17:23:48 -0500

    registered my service worker, added sample to uncomment

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
 self.addEventListener('fetch', function(event){
-
+ //empty for now!
+ // if you want to take a peak at all the page requests, uncomment the next line:
+ //console.log(event.request);
 });
