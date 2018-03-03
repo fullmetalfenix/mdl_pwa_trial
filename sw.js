5ded03cac0fef48cef3d0c60ea8783f14994af24
Jason <Jpihlk@yahoo.com>
2018-03-03 18:36:47 -0500

    Added SW Hi-jacking for all requests

--- sw.js
+++ sw.js
@@ -1,14 +1,18 @@
 self.addEventListener('fetch', function(event){
-// Hi-jack the requst! 
-event.respondWith(
-     new Response('<h1>Hello World</h1>', {
-         headers: {'Content-Type': "text/html"}
-     })
- );
- // want only .jpg's?
- //if(event,request.url.endsWith('.jpg')){
- //    event.respondWith(
- //        fetch('/imgs/dr-evil.gif')
- //    );
- // }
-});
+    // Hi-jack the requst! 
+    event.respondWith(
+        fetch(event.request).then(function(response){
+            if (response.status == 404) {
+              return new Response("Not Found! <a href='index.html'>Go Back</a>", {
+                headers: {'Content-Type': "text/html"}
+                });
+                //   Or erase that and respond with a proper 404 page:
+                //   event.respondWith(
+                //   fetch('404-page.html')
+            }
+            return response;
+        }).catch(function(){
+            return new Response("nope!")
+        })
+     );
+    });
