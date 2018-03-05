9b46f3f5dc0bd3409b55756aa7f5c7dc7335740d
Jason <Jpihlk@yahoo.com>
2018-03-04 22:25:45 -0500

    setting up the cache (work in progress)

--- sw.js
+++ sw.js
@@ -1,5 +1,27 @@
-self.addEventListener('fetch', function(event){
-    // Hi-jack the requst! 
+
+self.addEventListener('fetch', function(event){ //still on fetch so it updates more often (personal preference when debugging this part, should be install really)
+    event.waitUntil(
+        caches.open('coffee-4').then(function(cache){
+            return cache.addAll([
+              'index.html',
+              '5.jpg',
+              '6.jpg',
+              '7.jpg',
+              '10.jpg',
+              '14.jpg',
+              '16.jpg',
+              '21.jpg',
+              '22.jpg',
+              'coffee.jpg',
+              'index.html',
+              'styles.css',
+              'bower_components/jquery/dist/jquery.min.js',
+              'bower_components/material-design-lite/material.min.css',
+              'SVG/barista-icons_coffee-shop-sign.svg'
+            ])
+        })
+    );
+
     event.respondWith(
         fetch(event.request).then(function(response){
             if (response.status == 404) {
@@ -15,4 +37,9 @@ self.addEventListener('fetch', function(event){
             return new Response("nope!")
         })
      );
-    });
+
+
+   });
+   
+
+
